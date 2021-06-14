3599b4ed7aaebb6ab9089e10a2f5bcb61ecdf08a
MelikeF <z.melikefilizer@gmail.com>
2021-06-14 10:46:56 +0100

    update

---
+++ weath.js
@@ -0,0 +1,11 @@
+const key="f904640bab0ef7b2334cb44efa056b72";
+fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=f904640bab0ef7b2334cb44efa056b72");
+
+c
+const getCity = async(city) =>{
+
+}
+
+const getWeather = async(id) =>{
+ 
+}
