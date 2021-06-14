74def55bc272a8a3998962a4e7a529f3856a1d66
MelikeF <z.melikefilizer@gmail.com>
2021-06-14 11:47:34 +0100

    html changes

--- weath.js
+++ weath.js
@@ -1,11 +1,19 @@
+
+var search=document.querySelector("search")
+var button=document.querySelector("button")
+var inputs=document.querySelector("city")
+var temperat=document.querySelector("temperature")
+var descrips=document.querySelector("description")
+var tomweather=document.querySelector("tomtemp")
+
 const key="f904640bab0ef7b2334cb44efa056b72";
-fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=f904640bab0ef7b2334cb44efa056b72");
 
-c
-const getCity = async(city) =>{
 
-}
-
-const getWeather = async(id) =>{
- 
-}
+button.addEventListener("click",function(){
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+search.value+"&appid=f904640bab0ef7b2334cb44efa056b72")
+    .then(response=>response.json())
+    .then(data =>console.log("whatttt"))
+    
+.catch(err => alert("Some error is occured"))
+    
+})
